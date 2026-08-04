2959940444b663a5e771dc144334917eadae3236
ullasbc <ullasbc02@gmail.com>
2026-08-03 22:25:14 -0700

    Dynamically manipulate the DOM elements

--- taskflow/src/app/tasks/tasks.ts
+++ taskflow/src/app/tasks/tasks.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, finalize, tap } from 'rxjs/operators';
 import { ApiService } from '../services/api.service';
 import { Task } from '../models/task.model';
 
@@ -13,10 +14,26 @@ import { Task } from '../models/task.model';
 })
 export class Tasks implements OnInit {
   tasks$!: Observable<Task[]>;
+  private loadingSubject = new BehaviorSubject<boolean>(true);
+  loading$ = this.loadingSubject.asObservable();
+  errorMessage = '';
 
   constructor(private api: ApiService) {}
 
   ngOnInit(): void {
-    this.tasks$ = this.api.getTasks();
+    // console.log('Tasks component ngOnInit');
+    this.tasks$ = this.api.getTasks().pipe(
+      // tap(data => console.debug('API tasks response:', data)),
+      catchError(err => {
+        console.error('Tasks API error', err);
+        this.errorMessage = 'Failed to load tasks. Is the API running?';
+        return of([]); // fallback so the stream doesn't die
+      }),
+      finalize(() => this.loadingSubject.next(false))
+    );
+  }
+
+  trackById(index: number, task: Task): number {
+    return task.id;
   }
 }
