f01e07324fd622f2efd87e9bde5cf43a67f8c009
ullasbc <ullasbc02@gmail.com>
2026-08-24 17:33:23 -0700

    components: add dashboard-item component and update tickets and traffic components to use it

---
+++ 01-starting-project/src/app/dashboard/tickets/tickets.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DashboardItemComponent } from '../dashboard-item/dashboard-item.component';
+
+@Component({
+  selector: 'app-tickets',
+  standalone: true,
+  imports: [DashboardItemComponent],
+  templateUrl: './tickets.component.html',
+  styleUrl: './tickets.component.css'
+})
+export class TicketsComponent {
+
+}
